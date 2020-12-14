47183af0ad6c73893fb9c945ca800cf2132f190f
l31tm0t1f <siimsoonsein@gmail.com>
2020-12-14 16:09:53 +0200

    options menu and sound

---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Play sound globally
+    private static SoundScript instance = null;
+    
+    public static SoundScript Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject); // object target will not be destroyed when loading new scene
+    }
+    // Play Global End
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
